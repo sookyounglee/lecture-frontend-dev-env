0f16a69b93bc0dd41e5cd21556877c9b2b382aa4
sookyounglee <6831325@hanmail.net>
2023-01-09 21:03:55 +0900

    babel 강의용 소스

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 /**
  * TODO: IE11에서도 동작하는 자바스크립트로 변환할수 있게 바벨을 구성하세요.
  */
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          chrome: "79", // 크롬 79까지 지원하는 코드를 만든다
+          ie: "11", // ie 11까지 지원하는 코드를 만든다
+        },
+        useBuiltIns: "usage",
+        corejs: {
+          version: 2,
+        },
+      },
+    ],
+  ],
+};
